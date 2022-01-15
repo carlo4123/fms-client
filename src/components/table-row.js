49812c0eddb21fc1a22ac578eca8cbc71c217610
Carlo Cominguez <cominguez4123@gmail.com>
2022-01-15 10:51:19 +0800

    update ismd

--- src/components/table-row.js
+++ src/components/table-row.js
@@ -5,9 +5,10 @@ const TableRow = ({data, accountType}) => {
 const {units} = data
 // console.log("units",units)
 
- 
+
 //    const [modal1, setModal1] = useState(false)
   
+console.log()
  
     return ( 
         <tbody>
@@ -26,9 +27,15 @@ const {units} = data
             <td className="table-data">{data.vehicle}</td>
             <td className="table-data">from {unit.routeFrom} to {unit.routeTo}</td>
             <td className="table-data">{unit.yearModel}</td>
-            <td className="table-data"></td>
-            <td className="table-data"></td>
-            {accountType=== "admin_legal" &&
+           
+            <td className="table-data"> {data.tracking.length === 14 && "2022" }
+        
+            </td>
+            <td className="table-data"> {
+                  data.tracking.length === 14 && "2022"
+               }
+        </td>
+            {accountType === "admin_legal" || (accountType === "admin_ismd" && data.tracking.length === 10 ) &&
             <td className="table-data">
                <PrintDocu data={data} />
             </td>
